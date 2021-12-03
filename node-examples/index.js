b0c98681faae6e39a8cb38a94bff3c457f952aca
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2021-12-03 16:53:15 +0000

    node callbacks and error handling

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,14 +1,30 @@
 let rect = require('./rectangle');
 
+// synchronous function
+// function solveRect(l,b) {
+//     console.log("Solving for rectangle l = " + l + " and b = " + b);
+    
+//     if (l <= 0 || b <= 0){
+//         console.log("Rectangle dimesnions should be greater than 0 : " +" l = " + l + " and b = " + b); 
+//     } else {
+//         console.log("The area of the ractangle is " + rect.area(l,b));
+//         console.log("The perimeter of the ractangle is " + rect.perimeter(l,b));
+//     }
+// }
+
 function solveRect(l,b) {
     console.log("Solving for rectangle l = " + l + " and b = " + b);
-    
-    if (l <= 0 || b <= 0){
-        console.log("Rectangle dimesnions should be greater than 0 : " +" l = " + l + " and b = " + b); 
-    } else {
-        console.log("The area of the ractangle is " + rect.area(l,b));
-        console.log("The perimeter of the ractangle is " + rect.perimeter(l,b));
-    }
+
+    rect(l,b,(err, rectangle) => {
+        if(err){
+            console.log("ERROR", err.message);
+        } else {
+            console.log("The area of the rectangle of dimensions l = " + l + " and b = " + b + " is " + rectangle.area() + ".");
+            console.log("The perimeter of the rectangle of dimensions l = " + l + " and b = " + b + " is " + rectangle.perimeter() + ".");
+        }
+    });
+
+    console.log("This statement is after the call to rect()!");    
 }
 
 solveRect(2,4);
