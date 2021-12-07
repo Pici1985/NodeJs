f4d4041ff86b3f6288b0b8f5bcad890bcf882d4e
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2021-12-07 14:58:01 +0000

    node mongodb example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dbOper = require('./operations')
 
 const url = 'mongodb://localhost:27017'; 
 const dbname = 'conFusion'; 
@@ -12,23 +13,47 @@ MongoClient.connect(url, (err, client) => {
     const db = client.db(dbname);
     const collection = db.collection('dishes');
 
-    collection.insertOne({ "name": "Uthappizza", "description": "test"}, (err, result) => {
-        assert.equal(err, null); 
+    // practice method
+    // collection.insertOne({ "name": "Uthappizza", "description": "test"}, (err, result) => {
+    //     assert.equal(err, null); 
         
-        console.log('After insert: \n');
-        console.log(result.ops);
+    //     console.log('After insert: \n');
+    //     console.log(result.ops);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null); 
+    //     collection.find({}).toArray((err, docs) => {
+    //         assert.equal(err, null); 
             
-            console.log('Found: \n');
-            console.log(docs);
+    //         console.log('Found: \n');
+    //         console.log(docs);
             
-            db.dropCollection('dishes', (err, result) => {
-                assert.equal(err, null); 
+    //         db.dropCollection('dishes', (err, result) => {
+    //             assert.equal(err, null); 
 
-                client.close();
+    //             client.close();
+    //         });
+    //     });
+    // });
+
+    dbOper.insertDocument(db, { name: "Vadonut", description: "Test"}, 'dishes', (result) => {
+
+        console.log('Insert document:\n', result.ops);
+
+        dbOper.findDocuments(db, 'dishes', (docs) => {
+            console.log('Found documents:\n', docs);
+
+            dbOper.updateDocument(db, {name: 'Vadonut'}, {description: 'Updated test'}, 'dishes', (result) => {
+                console.log('Updated documet\n', result.result);
+
+                dbOper.findDocuments(db, 'dishes', (docs) => {
+                    console.log('Found documents:\n', docs);
+                    
+                    db.dropCollection('dishes', (result) => {
+                        console.log('Dropped Collection: ', result);
+
+                        client.close();
+                    });
+                });
             });
         });
-    });
+    });   
 });
